43efef8f32e0b32acdef48c1adf9a5062491d756
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-07 21:04:56 +0530

    LinkedList: Impl Node

---
+++ src/linked-list/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+module.exports = { Node, LinkedList };
